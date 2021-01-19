ec84761e59ee7313f3290f335d78b7abdb2cac0a
Kuldeep Chotaliya <kuldeepchotaliya17@gmail.com>
2021-01-19 10:57:35 +0530

    added one to many association to user to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_113608) do
+ActiveRecord::Schema.define(version: 2021_01_19_050021) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
+    t.integer "user_id"
   end
 
   create_table "user_tables", force: :cascade do |t|
